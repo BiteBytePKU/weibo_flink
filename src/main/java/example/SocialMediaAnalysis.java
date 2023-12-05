e488d7fb925868090d8fb00275d2c5b97c42e2de
BiteBytePKU <quanyuanjun@foxmail.com>
2023-12-05 13:27:35 +0800

    display version

--- src/main/java/example/SocialMediaAnalysis.java
+++ src/main/java/example/SocialMediaAnalysis.java
@@ -5,23 +5,23 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.util.Collector;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Properties;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
@@ -39,7 +39,6 @@ public class SocialMediaAnalysis {
         properties.setProperty("bootstrap.servers", "192.168.129.134:9092");
         properties.setProperty("group.id", "flink-group");
         String inputTopic = "weibo";
-        String outputTopic = "analysis";
 
         FlinkKafkaConsumer<String> consumer =
                 new FlinkKafkaConsumer<String>(inputTopic, new SimpleStringSchema(), properties);
@@ -51,48 +50,88 @@ public class SocialMediaAnalysis {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment(conf);
         */
 
-        // 读取微博数据
-//      DataStream<String> weiboStream = env.readTextFile("input/words.csv");
-
-        // 读取敏感词库文件
-        Set<String> sensitiveWords = readSensitiveWords("input/sensitive.csv");
+        // 读取关键词库文件
+        InputStream inputStream = SocialMediaAnalysis.class.getClassLoader().getResourceAsStream("sensitive.csv");
+        Set<String> sensitiveWords = readSensitiveWords(inputStream);
 
 
-        // 敏感词检测
+        // 关键词检测
         DataStream<Tuple3<String, String, String>> sensitiveWordsResult = weiboStream
                 .flatMap(new SensitiveWordChecker(sensitiveWords));
+
         // 情感倾向分析
-//        DataStream<Tuple3<String, String, String>> sentimentResult = weiboStream
-//                .map(new SentimentAnalyzer());
-//
-//        sentimentResult.print();
+        DataStream<Tuple3<String, String, String>> sentimentResult = weiboStream
+                .map(new SentimentAnalyzer());
 
-//        // 话题点赞数统计
-//        DataStream<Tuple4<String, Long, Long, Long>> topicLikesResult = weiboStream
-//                .flatMap(new TopicLikesExtractor())
-//                .keyBy(0) // 根据话题进行分组
-//                .timeWindow(Time.minutes(10)) // 10分钟的时间窗口
-//                .aggregate(new LikesAggregate());
 
-        sensitiveWordsResult.print();
-//        topicLikesResult.print();
+       // 微博点赞评论转发热度分析
+        DataStream<Tuple4<String, String,String,String>> topicLikesResult = weiboStream
+                .flatMap(new TopicLikesExtractor());
 
+        String outputTopic = "flinkresult";
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty("bootstrap.servers", "192.168.129.134:9091");
+
+        FlinkKafkaProducer<Tuple3<String, String, String>> myProducer1 = new FlinkKafkaProducer<>(
+                outputTopic,                  // 目标 Kafka 主题
+                new KafkaTuple3Schema(),        // 自定义的 Tuple3 到 String 的序列化 schema
+                properties                      // Kafka 配置属性
+        );
+        FlinkKafkaProducer<Tuple3<String, String, String>> myProducer2 = new FlinkKafkaProducer<>(
+                outputTopic,                  // 目标 Kafka 主题
+                new KafkaTuple3Schema(),        // 自定义的 Tuple3 到 String 的序列化 schema
+                properties                      // Kafka 配置属性
+        );
+        FlinkKafkaProducer<Tuple4<String, String,String, String>> myProducer3 = new FlinkKafkaProducer<>(
+                outputTopic,                  // 目标 Kafka 主题
+                new KafkaTuple4Schema(),        // 自定义的 Tuple4 到 String 的序列化 schema
+                properties                      // Kafka 配置属性
+        );
+
+        sensitiveWordsResult.writeAsCsv("result.csv");
+        // 将结果发送到 Kafka
+        sensitiveWordsResult.addSink(myProducer1).name("Kafka Sink - FlinkResult");
+        sentimentResult.addSink(myProducer2).name("Kafka Sink - FlinkResult");
+        topicLikesResult.addSink(myProducer3).name("Kafka Sink - FlinkResult");
         env.execute("Weibo Analysis");
+
     }
 
-    // 读取敏感词库
-    private static Set<String> readSensitiveWords(String filePath) throws IOException {
-        Set<String> sensitiveWords = new HashSet<>();
-        // 读取文件的单行内容
-        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-            stream.forEach(line -> sensitiveWords.addAll(Arrays.asList(line.split(","))));
+
+    public static class KafkaTuple3Schema implements SerializationSchema<Tuple3<String, String, String>> {
+        @Override
+        public byte[] serialize(Tuple3<String, String, String> element) {
+            // 自定义如何将 Tuple3 转换为字节数组
+            String result = element.f0 + "," + element.f1 + "," + element.f2;
+            return result.getBytes(StandardCharsets.UTF_8);
         }
-        // 使用逗号分割字符串，然后添加到集合中
-//        sensitiveWords.addAll(Arrays.stream(line.split(",")).collect(Collectors.toSet()));
+    }
+
+    public static class KafkaTuple4Schema implements SerializationSchema<Tuple4<String, String,String, String>> {
+        @Override
+        public byte[] serialize(Tuple4<String, String, String,String> element) {
+            // 自定义如何将 Tuple3 转换为字节数组
+            String result = element.f0 + "," + element.f1 + "," + element.f2+ "," + element.f3;
+            return result.getBytes(StandardCharsets.UTF_8);
+        }
+    }
+
+    private static Set<String> readSensitiveWords(InputStream inputStream) throws IOException {
+        Set<String> sensitiveWords = new HashSet<>();
+
+        // 使用 BufferedReader 从 InputStream 读取数据
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // 使用逗号分割字符串，然后添加到集合中
+                sensitiveWords.addAll(Arrays.asList(line.split(",")));
+            }
+        }
         return sensitiveWords;
     }
 
-    // 敏感词检测器
+
+    // 关键词检测器
     public static class SensitiveWordChecker implements FlatMapFunction<String, Tuple3<String, String, String>> {
         private Set<String> sensitiveWords;
 
@@ -104,15 +143,14 @@ public class SocialMediaAnalysis {
         public void flatMap(String value, Collector<Tuple3<String, String, String>> out) {
             try {
                 String[] columns = value.split(",");
-                System.out.println("__"+columns.length+"__"+value);
+//                System.out.println("__"+columns.length+"__"+value);
                 if (columns.length >= 3) {
                     String id = columns[0];
-                    String bid = columns[1];
                     String content = columns[2];
 
                     for (String word : sensitiveWords) {
                         if (content.contains(word)) {
-                            out.collect(new Tuple3<>(id, bid, word));
+                            out.collect(new Tuple3<>("关键词检测", "微博ID："+id, "命中关键词："+word));
                         }
                     }
                 }
@@ -139,15 +177,15 @@ public class SocialMediaAnalysis {
             String[] columns = value.split(",");
             if (columns.length >= 3) {
                 String id = columns[0];
-                String bid = columns[1];
                 String content = columns[2];
 
                 // 分析情感
                 String sentiment = analyzeSentiment(content);
 
-                return new Tuple3<>(id, bid, sentiment);
+                return new Tuple3<>("实时情感分析", "微博ID："+id, sentiment);
+//                return new Tuple3<>("实时情感分析", "微博ID："+id, selectedWord);
             } else {
-                return new Tuple3<>("", "", "无法分析");
+                return new Tuple3<>("实时情感分析", "", "内容无意义，无法分析");
             }
         }
 
@@ -179,40 +217,29 @@ public class SocialMediaAnalysis {
             }
         }
     }
+    // 热度分析
+    public static class TopicLikesExtractor implements FlatMapFunction<String, Tuple4<String, String, String,String>> {
 
-    // 话题和点赞数提取器
-    public static class TopicLikesExtractor implements FlatMapFunction<String, Tuple3<String, Long, Long>> {
         @Override
-        public void flatMap(String value, Collector<Tuple3<String, Long, Long>> out) {
+        public void flatMap(String value, Collector<Tuple4<String, String, String,String>> out) {
             String[] columns = value.split(",");
-            String topic = columns[3]; // 假设话题在第4列
-            long likes = Long.parseLong(columns[4]); // 假设点赞数在第5列
-            out.collect(new Tuple3<>(topic, likes, 1L));
-        }
-    }
-
-    // 对点赞数进行聚合的函数
-    public static class LikesAggregate implements AggregateFunction<Tuple3<String, Long, Long>, Tuple4<String, Long, Long, Long>, Tuple4<String, Long, Long, Long>> {
-        @Override
-        public Tuple4<String, Long, Long, Long> createAccumulator() {
-            return new Tuple4<>("", 0L, 0L, 0L); // 话题名称，总点赞数，最高点赞数，计数
-        }
-
-        @Override
-        public Tuple4<String, Long, Long, Long> add(Tuple3<String, Long, Long> value, Tuple4<String, Long, Long, Long> accumulator) {
-            return new Tuple4<>(value.f0, accumulator.f1 + value.f1, Math.max(accumulator.f2, value.f1), accumulator.f3 + 1);
-        }
-
-        @Override
-        public Tuple4<String, Long, Long, Long> getResult(Tuple4<String, Long, Long, Long> accumulator) {
-            long avgLikes = accumulator.f1 / accumulator.f3; // 计算平均点赞数
-            return new Tuple4<>(accumulator.f0, avgLikes, accumulator.f2, accumulator.f3);
+            if (columns.length >= 13) {
+                try {
+                    String id = columns[0];
+                    int likes = Integer.parseInt(columns[9]);
+                    int comments = Integer.parseInt(columns[10]);
+                    int reposts = Integer.parseInt(columns[11]);
+                    String topic = columns[12];
+                    double total = (likes + comments*1.1 + reposts*1.3)/500;
+                    if (total > 1) {
+                        out.collect(new Tuple4<>("热度分析", "微博ID："+id, "话题："+topic,"热度指数："+ total));
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Error parsing numbers: " + e.getMessage());
+                }
+            }
         }
 
-        @Override
-        public Tuple4<String, Long, Long, Long> merge(Tuple4<String, Long, Long, Long> a, Tuple4<String, Long, Long, Long> b) {
-            return new Tuple4<>(a.f0, a.f1 + b.f1, Math.max(a.f2, b.f2), a.f3 + b.f3);
-        }
     }
 
 }
